d0a05f426651f20a1fd1befca02351ff0f4daa27
hanna-guta <161453575+hanna-guta@users.noreply.github.com>
2024-02-29 14:53:42 +0300

    Add solution for Maximum Difference Between Node and Ancestor

---
+++ camp/leetcode/maximum-difference-between-node-and-ancestor.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        max_diff = 0
+
+        def dfs(node , min_, max_ ):
+            nonlocal max_diff
+
+            if not node:
+                return 0
+
+            min_ = min(min_, node.val)
+            max_ = max(max_, node.val)
+            max_diff = max(max_diff , max_ - min_)
+
+            dfs(node.left , min_ , max_)
+            dfs(node.right , min_ ,max_)
+
+        dfs(root , root.val , 0)
+
+        return max_diff
+
+
+
+
+            
+
+
+
+
+        
